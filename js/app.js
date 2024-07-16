c601400460a7ec506076a2a78d0e3e9dd6d2dc57
techbyadi <pssharma.aditya@gmail.com>
2024-07-16 09:53:38 -0700

    complete assignment

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 /*-------------------------------- Constants --------------------------------*/
-
-//console.log('I am app.js');
 const winningCombos = [
   [0, 1, 2],
   [3, 4, 5],
@@ -10,8 +8,6 @@ const winningCombos = [
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6]];
-
-
 
 /*---------------------------- Variables (state) ----------------------------*/
 let board, turn, winner, tie;
@@ -39,8 +35,7 @@ function init() {
   render();
 }
 
-function render(){
-  //console.log('I am a render function');
+function render() {
   updateBoard();
   updateMessage();
 }
@@ -62,9 +57,9 @@ function updateBoard() {
 }
 
 function updateMessage() {
-  if(winner === false && tie === false){
+  if (winner === false && tie === false) {
     messageEl.textContent = `It is ${turn}'s turn`;
-  } else if (winner === false && tie === true){
+  } else if (winner === false && tie === true) {
     messageEl.textContent = `Cat's game. It's a tie!`;
   } else {
     messageEl.textContent = `${turn} wins the game`;
@@ -72,59 +67,48 @@ function updateMessage() {
 }
 
 
-function handleClick(event){
-    //console.log(event.target);
-    const squareIndex = parseInt(event.target.id);
-    if(board[squareIndex] === 'X' || board[squareIndex] === 'O' || winner){
-      return;
-    }
-    placePiece(squareIndex);
-    //check for winner
-    checkForWinner();
-    //console.log('Winner:', winner);
-    //check for tie
-    checkForTie();
-    //change the turn
-    switchPlayerTurn();
-    render();
+function handleClick(event) {
+  //console.log(event.target);
+  const squareIndex = parseInt(event.target.id);
+  if (board[squareIndex] === 'X' || board[squareIndex] === 'O' || winner) {
+    return;
+  }
+  placePiece(squareIndex);
+  //check for winner
+  checkForWinner();
+  //check for tie
+  checkForTie();
+  //change the turn
+  switchPlayerTurn();
+  render();
 }
 
-function placePiece(index){
+function placePiece(index) {
   board[index] = turn;
   console.log(board);
 }
 
+
 function checkForWinner() {
-  if (
-    (board[0] !== '' && board[0] === board[1] && board[0] === board[2]) ||
-    (board[3] !== '' && board[3] === board[4] && board[3] === board[5]) ||
-    (board[6] !== '' && board[6] === board[7] && board[6] === board[8]) ||
-    (board[0] !== '' && board[0] === board[3] && board[0] === board[6]) ||
-    (board[1] !== '' && board[1] === board[4] && board[1] === board[7]) ||
-    (board[2] !== '' && board[2] === board[5] && board[2] === board[8]) ||
-    (board[0] !== '' && board[0] === board[4] && board[0] === board[8]) ||
-    (board[2] !== '' && board[2] === board[4] && board[2] === board[6])) {
-    winner = true;
-  }
+  winningCombos.forEach((combination) => {
+    if (board[combination[0]] !== '' && board[combination[0]] === board[combination[1]] && board[combination[0]] === board[combination[2]]) {
+      winner = true;
+    }
+  })
 }
 
-function checkForTie(){
-  if(winner) return;
-  if(!board.includes('')){
+
+function checkForTie() {
+  if (winner) return;
+  if (!board.includes('')) {
     tie = true;
   }
 }
 
-function switchPlayerTurn(){
+function switchPlayerTurn() {
   if (winner) {
     return;
-  } 
-  // if (turn === 'X') {
-  //   turn = 'O';
-  // }
-  // else {
-  //   turn = 'X';
-  // }
+  }
   turn = turn === 'X' ? 'O' : 'X';
 }
 
@@ -132,24 +116,10 @@ function switchPlayerTurn(){
 
 
 squareEls.forEach((squareEl) => {
-    squareEl.addEventListener('click', handleClick); 
+  squareEl.addEventListener('click', handleClick);
 })
 
 resetButtonEl.addEventListener('click', init);
 
-//1) Define the required variables used to track the state of the game.
-
-//2) Store cached element references.
-
-//3) Upon loading, the game state should be initialized, and a function should 
-//   be called to render this game state.
-
-//4) The state of the game should be rendered to the user.
-
-//5) Define the required constants.
-
-//6) Handle a player clicking a square with a `handleClick` function.
-
-//7) Create Reset functionality.
 
 
